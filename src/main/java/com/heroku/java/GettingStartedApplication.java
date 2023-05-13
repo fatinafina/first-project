1b47b5e496f7892fab09d5bb467e395667181e7b
fatinafina <2021899216@student.uitm.edu.my>
2023-05-14 01:24:22 +0800

    Use ENERGY environment variable

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -54,12 +54,16 @@ public class GettingStartedApplication {
 @GetMapping("/convert")
 String convert(Map<String, Object> model) {
     RelativisticModel.select();
-    var energy = Amount.valueOf("12 GeV");
 
-    model.put("result", "E=mc^2: " + energy + " = " + energy.to(SI.KILOGRAM));
+    final var result = java.util.Optional
+            .ofNullable(System.getenv().get("ENERGY"))
+            .map(Amount::valueOf)
+            .map(energy -> "E=mc^2: " + energy + " = " + energy.to(SI.KILOGRAM))
+            .orElse("ENERGY environment variable is not set!");
+
+    model.put("result", result);
     return "convert";
 }
-
     public static void main(String[] args) {
         SpringApplication.run(GettingStartedApplication.class, args);
     }
